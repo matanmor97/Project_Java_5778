557659e9236bcb8624fe83fdff9f58da8e5fdf59
matanmor97 <34842406+matanmor97@users.noreply.github.com>
2017-12-26 18:19:35 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/user/project_java_5778/controller/MainActivity.java
+++ app/src/main/java/com/example/user/project_java_5778/controller/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Toast.makeText(getBaseContext(),"abb",Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(),"abc",Toast.LENGTH_LONG).show();
 
     }
 }
