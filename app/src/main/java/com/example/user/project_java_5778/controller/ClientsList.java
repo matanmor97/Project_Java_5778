75d71376b32d8a07fd8797380695e0a6fede3ec9
matan <mmmor97@gmail.com>
2018-01-03 22:36:17 +0200

    matan 5.4 ClientList

---
+++ app/src/main/java/com/example/user/project_java_5778/controller/ClientsList.java
@@ -0,0 +1,15 @@
+package com.example.user.project_java_5778.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.user.project_java_5778.R;
+
+public class ClientsList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clients_list);
+    }
+}
