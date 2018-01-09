6a8387946b1e297e0ebc570fe4749ab873e91176
matan <mmmor97@gmail.com>
2018-01-09 15:37:13 +0200

    matan 6.1.4.2

--- app/src/main/java/com/example/user/project_java_5778/controller/ClientsList.java
+++ app/src/main/java/com/example/user/project_java_5778/controller/ClientsList.java
@@ -78,11 +78,11 @@ public class ClientsList extends Activity implements View.OnClickListener  {
 
         //show in every item on the viewList the Client ToString implementation
         //AsyncTask<Void, Integer, Void>
-        //adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,instance.getClients());
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,instance.getClients());
 
         //show in every item on the viewList the row_item.xml
         //don't working!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        adapter = new ArrayAdapter<Client>(this,R.layout.row_item,instance.getClients())
+        /*adapter = new ArrayAdapter<Client>(this,R.layout.row_item,instance.getClients())
         {
             @Override
             public View getView(int position, View convertView, ViewGroup parent)
@@ -96,14 +96,14 @@ public class ClientsList extends Activity implements View.OnClickListener  {
                         .findViewById(R.id.idTextView);
                 TextView productNameTextView = (TextView) convertView
                         .findViewById(R.id.nameTextView);
-                ImageView productionImageView = (ImageView) convertView
-                        .findViewById(R.id.image_view);
+                //ImageView productionImageView = (ImageView) convertView
+                //        .findViewById(R.id.image_view);
                 productIdTextView.setText((int)instance.getClients().get(position).getId());
                 productNameTextView.setText(instance.getClients().get(position).getFirstName() + " , " + instance.getClients().get(position).getLastName());
 
                 return convertView;
                  }
-        };
+        };*/
 
 
         clientList.setAdapter(adapter);
