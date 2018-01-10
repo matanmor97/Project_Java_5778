9cc829b5514a7ae214fe0b4f40f471e70511cdc1
user <aviadkorn@gmail.com>
2018-01-10 15:41:52 +0200

    aviad 7.2

--- app/src/main/java/com/example/user/project_java_5778/controller/MainActivity.java
+++ app/src/main/java/com/example/user/project_java_5778/controller/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button branches;
     private Button carModels;
     private Button cars;
-    int a;
 
     /**
      * Find the Views in the layout
